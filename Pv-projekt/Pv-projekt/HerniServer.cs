bee1cf32fec139cf4dd1b025bdfb5950d9c55402
misasypecky-arch <misa.sypecky@gmail.com>
2026-03-29 15:07:22 +0200

    Add files via upload

---
+++ Pv-projekt/Pv-projekt/HerniServer.cs
@@ -0,0 +1,45 @@
+﻿namespace Pv_projekt;
+
+using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+public class HerniServer
+{
+    private TcpListener _listener;
+    
+    
+    public ConcurrentDictionary<Guid, KlientSession> PripojeniKlienti { get; } = new();
+
+    public async Task StartAsync(int port)
+    {
+        _listener = new TcpListener(IPAddress.Any, port);
+        _listener.Start();
+        Console.WriteLine($"[Server] Peklo otevřelo své brány na portu {port}...");
+
+        while (true)
+        {
+            
+            TcpClient tcpClient = await _listener.AcceptTcpClientAsync();
+            Console.WriteLine("[Server] Nová duše vstoupila (nový klient připojen)!");
+
+            
+            Guid clientId = Guid.NewGuid();
+            var session = new KlientSession(tcpClient, clientId, toto_odeber_klienta);
+            
+            PripojeniKlienti.TryAdd(clientId, session);
+
+            
+            _ = Task.Run(() => session.ZpracujKomunikaciAsync());
+        }
+    }
+
+    
+    private void toto_odeber_klienta(Guid clientId)
+    {
+        PripojeniKlienti.TryRemove(clientId, out _);
+        Console.WriteLine($"[Server] Klient {clientId} se odpojil.");
+    }
+}
---
+++ Pv-projekt/Pv-projekt/KlientSession.cs
@@ -0,0 +1,81 @@
+﻿namespace Pv_projekt;
+
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+public class KlientSession
+{
+    private CommandHandler commandHandler;
+    private Hrac hrac;
+    private readonly TcpClient _client;
+    private readonly Guid _clientId;
+    private readonly Action<Guid> _onDisconnect;
+    
+    private StreamReader _reader;
+    private StreamWriter _writer;
+
+    public KlientSession(TcpClient client, Guid clientId, Action<Guid> onDisconnect)
+    {
+        _client = client;
+        _clientId = clientId;
+        _onDisconnect = onDisconnect;
+        
+        var stream = _client.GetStream();
+        _reader = new StreamReader(stream);
+        _writer = new StreamWriter(stream) { AutoFlush = true }; 
+    }
+
+    public async Task ZpracujKomunikaciAsync()
+    {
+        await PosliZpravuAsync("Vítej v Pekle! Zadej svůj příkaz (např. 'jdi sever'):");
+
+        try
+        {
+            while (_client.Connected)
+            {
+                
+                string zpravaOdHrace = await _reader.ReadLineAsync();
+                
+                
+                if (zpravaOdHrace == null) break;
+
+                Console.WriteLine($"[Klient {_clientId}] poslal: {zpravaOdHrace}");
+
+                
+                 string odpoved = commandHandler.ProcessCommand(hrac, zpravaOdHrace);
+                 await PosliZpravuAsync(odpoved);
+
+                
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[Chyba] {_clientId}: {ex.Message}");
+        }
+        finally
+        {
+            UkoniSpojeni();
+        }
+    }
+
+    public async Task PosliZpravuAsync(string zprava)
+    {
+        try
+        {
+            await _writer.WriteLineAsync(zprava);
+        }
+        catch
+        {
+            
+            UkoniSpojeni();
+        }
+    }
+
+    private void UkoniSpojeni()
+    {
+        _client.Close();
+        _onDisconnect?.Invoke(_clientId);
+    }
+}
