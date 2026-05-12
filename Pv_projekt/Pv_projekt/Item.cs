65cc32bf4edf9c877aaafa57042248219a51e56f
misasypecky-arch <misa.sypecky@gmail.com>
2026-05-12 22:46:51 +0200

    Add files via upload

---
+++ Pv_projekt/Pv_projekt/Item.cs
@@ -0,0 +1,60 @@
+namespace Pv_projekt;
+
+public class Item
+{
+    public string ItemId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ItemType { get; set; } 
+    public int Value { get; set; }
+    
+    
+    public string EffectId { get; set; }
+
+    public Item(string itemId, string name, string description, string itemType, int value = 0, string effectId = "nic")
+    {
+        ItemId = itemId;
+        Name = name;
+        Description = description;
+        ItemType = itemType;
+        Value = value;
+        EffectId = effectId; 
+    }
+
+    
+    public string VykonejEfekt(Hrac hrac)
+    {
+        if (EffectId == "nic") 
+            return $"Použil jsi {Name}, ale vůbec nic se nestalo.";
+
+        if (EffectId == "heal_30")
+        {
+            hrac.Hp = Math.Min(100, hrac.Hp + 30);
+            return $"Vypil jsi {Name} a obnovil si 30 HP.";
+        }
+        
+        if (EffectId == "def_3")
+        {
+            hrac.Defense += 3;
+            return $"Použil jsi {Name}. Tvá obrana se dočasně zvýšila (+3).";
+        }
+
+        if (EffectId == "revolver_rng")
+        {
+            Random rng = new Random();
+            if (rng.Next(1, 7) == 1) 
+            {
+                hrac.Hp -= 50;
+                hrac.Faith -= 20;
+                return "CVAK... BUM! Revolver vystřelil. Tvé HP a Víra prudce klesly!";
+            }
+            else
+            {
+                hrac.Faith += 5;
+                return "CVAK... Měl jsi štěstí. Tvá Víra v přežití roste (+5 Faith).";
+            }
+        }
+
+        return $"Neznámý efekt předmětu {Name}.";
+    }
+}
